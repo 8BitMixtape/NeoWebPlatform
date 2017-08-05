e89be88e92206ecff08c31380e337098539c0b97
Budi Prakosa <iyok@deadmediafm.org>
2017-08-05 17:29:41 +0700

    add exchange resource

--- app/Http/Controllers/ExchangeController.php
+++ app/Http/Controllers/ExchangeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Repositories\MixtapeRepository;
 use App\Http\Middleware\IsAjax;
+use App\Http\Requests\MixtapeCreateRequest;
+
 
 class ExchangeController extends Controller
 {
@@ -52,5 +54,63 @@ class ExchangeController extends Controller
     {
         return view('exchange.editprofile');
     }
+
+
+    /**
+     * Store a newly created post in storage.
+     *
+     * @param  \App\Http\Requests\PostRequest $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(MixtapeCreateRequest $request)
+    {
+        print_r($request->all());
+        // $this->blogRepository->store($request->all(), $request->user()->id);
+        // return redirect('blog')->with('ok', trans('back/blog.stored'));
+    }
+
+
+    /**
+     * Show the form for editing the specified post.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        // $post = $this->blogRepository->getByIdWithTags($id);
+        // $this->authorize('change', $post);
+        // return view('back.blog.edit', $this->blogRepository->getPostWithTags($post));
+    }
+
+    /**
+     * Update the specified post in storage.
+     *
+     * @param  \App\Http\Requests\PostUpdateRequest $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(PostRequest $request, $id)
+    {
+        // $post = $this->blogRepository->getById($id);
+        // $this->authorize('change', $post);
+        // $this->blogRepository->update($request->all(), $post);
+        // return redirect('blog')->with('ok', trans('back/blog.updated'));
+    }
+
+    /**
+     * Remove the specified post from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // $post = $this->blogRepository->getById($id);
+        // $this->authorize('change', $post);
+        // $this->blogRepository->destroy($post);
+        // return redirect('blog')->with('ok', trans('back/blog.destroyed'));
+    }
+
 
 }
