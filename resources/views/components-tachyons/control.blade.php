d27e997173d56d6191e940ba9cccbad62ac55425
Budi Prakosa <iyok@deadmediafm.org>
2017-08-05 17:28:38 +0700

    add name

--- resources/views/components-tachyons/control.blade.php
+++ resources/views/components-tachyons/control.blade.php
@@ -1,13 +1,13 @@
 <div class="measure {{ $class }} {{ $errors->has($name) ? 'has-error' : '' }}">
     @if($type == 'textarea')
         <label for="{{ $label }}" class="f6 b db mb2">{{ $name }} <span class="normal black-60">{{ $required }}</span></label>
-        <textarea id="{{ $label }}" name="{{ $name }}" class="db border-box hover-black w-100 ba b--black-20 pa2 br2 mb2" aria-describedby="{{ $name }}-desc">{{ $value }}</textarea>    
+        <textarea id="{{ $label }}" name="{{ $label }}" class="db border-box hover-black w-100 ba b--black-20 pa2 br2 mb2" aria-describedby="{{ $name }}-desc">{{ $value }}</textarea>    
     @elseif($type == 'file')
         <label for="{{ $label }}" class="f6 b db mb2">{{ $name }} <span class="normal black-60">{{ $required }}</span></label>
-        <input id="{{ $label }}" class="input-reset ba b--black-20 pa2 mb2 db w-100" type="file" value="{{ $value }}" aria-describedby="{{ $name }}-desc">
+        <input id="{{ $label }}" name="{{ $label }}" class="input-reset ba b--black-20 pa2 mb2 db w-100" type="file" value="{{ $value }}" aria-describedby="{{ $name }}-desc">
     @else    
         <label for="{{ $label }}" class="f6 b db mb2">{{ $name }} <span class="normal black-60">{{ $required }}</span></label>
-        <input id="{{ $label }}" class="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" value="{{ $value }}" aria-describedby="{{ $name }}-desc">
+        <input id="{{ $label }}" name="{{ $label }}" class="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" value="{{ $value }}" aria-describedby="{{ $name }}-desc">
     @endif
 
     @if($errors->has($name))
