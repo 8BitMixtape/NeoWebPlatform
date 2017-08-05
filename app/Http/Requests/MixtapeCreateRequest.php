1249c190c0fe7025608d63fa613e2694334c11bd
Budi Prakosa <iyok@deadmediafm.org>
2017-08-05 17:29:12 +0700

    update request

--- app/Http/Requests/MixtapeCreateRequest.php
+++ app/Http/Requests/MixtapeCreateRequest.php
@@ -2,20 +2,8 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
-
-class MixtapeCreateRequest extends FormRequest
+class MixtapeCreateRequest extends Request
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return false;
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *
